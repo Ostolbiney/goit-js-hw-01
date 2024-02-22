e2758185164fd65732c0265889fe670d8263e871
Ostolbiney <152024544+Ostolbiney@users.noreply.github.com>
2024-02-22 22:04:14 +0200

    Changed command

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,8 @@
 "use strict";
 function makeTransaction(quantity, pricePerDroid) {
+  let totalPrice = quantity * pricePerDroid;
   return `You ordered ${quantity} droids worth ${pricePerDroid} credits!`;
 }
-makeTransaction(5, 3000);
-makeTransaction(3, 1000);
-makeTransaction(10, 500);
+console.log(makeTransaction(5, 3000));
+console.log(makeTransaction(3, 1000));
+console.log(makeTransaction(10, 500));
